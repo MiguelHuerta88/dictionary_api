d3a34bf72267566c6f1b57d907254c9f2f0e99b7
Miguel Huerta <miguelhuerta@Miguels-MacBook-Pro.local>
2018-11-18 08:37:50 -0800

    Created Words model along with migration

---
+++ app/Models/Words.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Words extends Model
+{
+	/**
+	 * Table to be used
+	 *
+	 * @var string
+	 */
+    protected $table = 'words'
+}
